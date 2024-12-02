f9a8a3048c604d7a2655b97ff8274add2ccc0055
chhabrasaurabh80 <105360203+chhabrasaurabh80@users.noreply.github.com>
2024-12-02 21:46:38 +0530

    Update GoogleSearchTest.java

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -13,8 +13,9 @@ public class GoogleSearchTest extends BaseTest {
         driver.findElement(By.name("q")).sendKeys("Selenium WebDriver");
         driver.findElement(By.name("q")).submit();
 
-        // Assertion 1: Verify the page title contains "Selenium WebDriver"
-        Assert.assertTrue(driver.getTitle().contains("Selenium WebDriver"), "Page title does not contain 'Selenium WebDriver'");
+        // Assertion 1: Verify the page contains link "selenium.dev"
+        WebElement link= driver.findElement(By.xpath("(//a[contains(@href,'www.selenium.dev/')])[1]"));
+        Assert.assertTrue(link.isDisplayed(), "Page does not search 'selenium.dev'");
 
         // Assertion 2: Verify the Google logo is displayed
         Assert.assertTrue(driver.findElement(By.id("logo")).isDisplayed(), "Google logo is not displayed");
