c70262f1d75dad2c79a85fff3cba9ef61f54a10f
chhabrasaurabh80 <105360203+chhabrasaurabh80@users.noreply.github.com>
2024-12-03 00:06:27 +0530

    Update GoogleSearchTest.java

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -10,14 +10,17 @@ public class GoogleSearchTest extends BaseTest {
 
     @Test
     public void testGoogleSearch() {
+        Allure.step("Validated the first link is-google.com ");
         driver.get("https://www.google.com");
         driver.findElement(By.name("q")).sendKeys("Selenium WebDriver");
         driver.findElement(By.name("q")).submit();
 
+        Allure.step("Validated the first link is-selenium.dev ");
         // Assertion 1: Verify the page contains link "selenium.dev"
         WebElement link= driver.findElement(By.xpath("(//a[contains(@href,'www.selenium.dev/')])[1]"));
         Assert.assertTrue(link.isDisplayed(), "Page does not search 'selenium.dev'");
 
+        Allure.step("Validated the Google Logo displyed ");
         // Assertion 2: Verify the Google logo is displayed
         Assert.assertTrue(driver.findElement(By.id("logo")).isDisplayed(), "Google logo is not displayed");
     }
